6fda421118e161d1bbf61adfffb0766808f96356
hk0310 <42828125+hk0310@users.noreply.github.com>
2022-11-02 14:59:27 -0500

    First commit

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php 
+    // Establishes a connection to the database.
+
+    define('DB_DSN', 'mysql:host=localhost;dbname=serverside;charset=utf8');
+    define('DB_USER', 'serveruser');
+    define('DB_PASS', 'gorgonzola7!');
+
+    try {
+        $db = new PDO(DB_DSN, DB_USER, DB_PASS);
+    } 
+    catch(PDOException $e) {
+        print "Error: " . $e->getMessage();
+        die();
+    }
+?>
