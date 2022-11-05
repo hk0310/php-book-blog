5396820faba1a7b835556c22632795d1d2d7cea5
hk0310 <42828125+hk0310@users.noreply.github.com>
2022-11-04 21:01:53 -0500

    Implemented the logout functionality.

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+
+    if(isset($_SESSION["user"])) {
+        session_destroy();
+        header("Location: index.php");
+        exit();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error</title>
+</head>
+<body>
+    <p>Page not found.</p>
+</body>
+</html>
