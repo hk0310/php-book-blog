cb20d655673338ac7284200bcd7d502f275303d6
hk0310 <42828125+hk0310@users.noreply.github.com>
2022-11-05 00:37:09 -0500

    Restructured the back end of the Register page so that multiple errors can be displayed beneath the field.

--- register.php
+++ register.php
@@ -2,11 +2,10 @@
     require("connect.php");
 
     $errorFlag = false;
-    $userDuplicate = "";
-    $fieldsToValidate = ['username' => ['hasError' => false,],
-                         'email' => ['hasError' => false],
-                         'password' => ['hasError' => false],
-                         'birthdate' => ['hasError' => false]
+    $fieldsToValidate = ['username' => ['isEmpty' => false, 'name' => 'Username'],
+                         'email' => ['isEmpty' => false, 'name' => 'Email'],
+                         'password' => ['isEmpty' => false, 'name' => 'Password'],
+                         'passwordConfirm' => ['isEmpty' => false, 'name' => 'Password Confirmation'], 
                         ];
 
     if(isset($_POST['button'])) {
@@ -14,40 +13,51 @@
             if(!isset($_POST[$field]) || (empty(trim($_POST[$field]))))
             {
                 $errorFlag = true;
-                $fieldsToValidate[$field]['hasError'] = true;
+                $fieldsToValidate[$field]['isEmpty'] = true;
             }
         }
         if(!$errorFlag) {
-            $userDuplicate = SubmitToDatabase($db);
+            $additionalErrors = SubmitToDatabase($db);
         }
     }
 
     function SubmitToDatabase($db) {
+        $errors = [];
+
         $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $password = password_hash($password, PASSWORD_BCRYPT, ['cost' => 13]);
-        $birthdate = filter_input(INPUT_POST, "birthdate", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $passwordConfirm = filter_input(INPUT_POST, "passwordConfirm", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
 
-        $testDate = date_create($birthdate);
+        $userAvailableQuery = "SELECT username FROM Users";
+        $userAvailableStatement = $db->prepare($userAvailableQuery);
+        $userAvailableStatement->execute();
 
-        if(!$testDate || !$email) {
-            return;
+        while($usernameDB = $userAvailableStatement->fetch()) {
+            if($username == $usernameDB['username']) {
+                $errors['username'] = "Username has already been taken.";
+            }
         }
-        
-        try {
-            $query = "INSERT INTO Users (username, password, role, email, birthdate) VALUES (:username, :password, 0, :email, :birthdate)";
-            $statement = $db->prepare($query);
-            $statement->bindValue(':username', $username);
-            $statement->bindValue(':password', $password);
-            $statement->bindValue(':email', $email);
-            $statement->bindValue(':birthdate', $birthdate);
-            $statement->execute();
+        if(!$email) {
+            $errors['email'] = "The provided email was invalid";
         }
-        catch(Exception $e) {
-            return "Username has already been taken.";
+        if($password !== $passwordConfirm) {
+            $errors['password'] = "Passwords do not match";
         }
 
+        if(!empty($errors)) {
+            return $errors;
+        }
+
+        $query = "INSERT INTO Users (username, password, role, email) VALUES (:username, :password, 1, :email)";
+        $statement = $db->prepare($query);
+
+        $password = password_hash($password, PASSWORD_BCRYPT, ['cost' => 15]);
+
+        $statement->bindValue(':username', $username);
+        $statement->bindValue(':password', $password);
+        $statement->bindValue(':email', $email);
+        $statement->execute();
 
         header('Location: login.php');
         exit();
@@ -66,10 +76,10 @@
 
     <form action="register.php" method="post">
         <?php if($errorFlag): ?>
-            <p>The following fields were invalid:</p>
+            <p>The following fields cannot be empty:</p>
             <ul>
                 <?php foreach($fieldsToValidate as $field => $info): ?>
-                    <?php if($info['hasError']): ?>
+                    <?php if($info['isEmpty']): ?>
                         <li><?= $field ?></li>
                     <?php endif ?>
                 <?php endforeach ?>
@@ -78,18 +88,24 @@
         <fieldset>
             <label for="username">Username</label>
             <input type="text" id="username" name="username" required>
-            <?php if(!empty($userDuplicate)): ?>
-                <p><?= $userDuplicate ?></p>
+            <?php if(isset($additionalErrors['username'])): ?>
+                <p><?= $additionalErrors['username'] ?></p>
             <?php endif ?>
 
             <label for="email">Email</label>
             <input id="email" name="email" type="email" required> 
+            <?php if(isset($additionalErrors['email'])): ?>
+                <p><?= $additionalErrors['email'] ?></p>
+            <?php endif ?>
 
             <label for="password">Password</label>
             <input id="password" name="password" type="password" required>
 
-            <label for="birthdate">Date of Birth</label>
-            <input id="birthdate" name="birthdate" type="date" required>
+            <label for="passwordConfirm">Re-enter Password</label>
+            <input id="passwordConfirm" name="passwordConfirm" type="password" required>
+            <?php if(isset($additionalErrors['password'])): ?>
+                <p><?= $additionalErrors['password'] ?></p>
+            <?php endif ?>
 
             <p>
                 <button type="submit" value="register" name="button">Register</button>
