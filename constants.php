f4d7401c9dd88ef77126da019cc83c95c689d558
hk0310 <42828125+hk0310@users.noreply.github.com>
2022-11-08 23:28:49 -0600

    Added the constant.php file containing most used constants.

---
+++ constants.php
@@ -0,0 +1,6 @@
+<?php
+    define("DS", DIRECTORY_SEPARATOR);
+    define("ROOT", __DIR__);
+    define("CONNECT_PATH", ROOT . DIRECTORY_SEPARATOR . "misc" . DIRECTORY_SEPARATOR . "connect.php");
+    define("HEADER_PATH", ROOT . DIRECTORY_SEPARATOR . "misc" . DIRECTORY_SEPARATOR . "header.php");
+?>
